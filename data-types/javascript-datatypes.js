d356b5d6e9ecf3e0e88c186ad10e5e2ecc33cbd1
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-15 20:28:42 +0900

    Javascript Database Program

---
+++ data-types/javascript-datatypes.js
@@ -0,0 +1,14 @@
+const number1 = 3;
+const number2 = 3.2343;
+const number3 = 3e5
+const student =
+                {
+                    firstname: 'Nishant',
+                    lastname: 'Meher',
+                    course: '情報修理ネットワーク'
+                };
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(student);
+
