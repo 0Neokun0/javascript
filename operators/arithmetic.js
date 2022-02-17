35bc2c93a30359e46d3c71813923825dcbd519da
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-17 11:29:36 +0900

    Arithmetic Javascript Program

---
+++ operators/arithmetic.js
@@ -0,0 +1,24 @@
+// Variables
+
+let number1 = 5;
+let number2 = 3;
+
+// Addition
+console.log('5 + 3 = ' , number1 + number2);
+
+// Subtraction
+console.log('5 - 3 = ' , number1 - number2);
+
+// Multiplication
+console.log('5 * 3 = ' , number1 * number2);
+
+// Division
+console.log('5 / 3 = ' , number1 / number2);
+
+// Remainder
+console.log('5 % 3 = ' , number1 % number2);
+
+// Exponentiation
+console.log('5 ** 3 = ', number1 ** number2);
+
+// End
