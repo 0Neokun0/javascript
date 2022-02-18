4109d8fe7c4dfb3c94b8fd8f064ad3a758c3f616
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-18 10:28:39 +0900

    Javascript If..Else Program

---
+++ if-else/if_else.js
@@ -0,0 +1,23 @@
+/*
+Check if the number is 
+Positive
+Negative
+or
+Zero */
+
+const number = prompt("Enter a number: ");
+
+if (number >= 0) 
+{
+    if (number == 0) 
+    {
+        console.log("You entered number 0");
+    } else 
+    {
+        console.log("You entered a positive number");
+    }
+} 
+else 
+{
+    console.log("You entered a negative number");
+}
