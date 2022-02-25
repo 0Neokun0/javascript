662123460df137fae102babd81ea2e3f4062961a
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-25 17:28:08 +0900

    Javascript Methods Program:
    clear
    
    clear

---
+++ javascript-methods/methods.js
@@ -0,0 +1,12 @@
+const person =
+{
+    name : 'Nishant',
+    age : 26,
+    greet : function()
+    {
+        let surname = 'Meher';
+        console.log('My name is' + ' ' + this.name + ' ' + surname);
+    }
+};
+
+person.greet();
