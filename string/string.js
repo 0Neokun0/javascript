d8f7c713f5c6e2db5e6ffd7f1de3f2bc0fedcc60
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-27 20:02:03 +0900

    String Javascript Program

---
+++ string/string.js
@@ -0,0 +1,8 @@
+const string1 = 'hello';
+const string2 = new String('hello');
+
+console.log(string1);
+console.log(string2);
+
+console.log("String Type : " + typeof string1);
+console.log("String Type : " +typeof string2);
