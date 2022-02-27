bccdbefbff86a505ec2fc1fe08f2c9634b271088
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-28 07:29:33 +0900

    Getter-Setter Javascript Program

---
+++ getter-setter/getter_setter.js
@@ -0,0 +1,25 @@
+// Variable
+const student = {
+    firstName: "0Neokun0"
+}
+
+// Getter Property
+Object.defineProperty( student, "getName", {
+    get : function () {
+        return this.firstName;
+    }
+});
+
+// Setter Property
+Object.defineProperty(student, "changeName", {
+    set : function (value) {
+        this.firstName = value;
+    }
+});
+
+console.log(student.firstName); // 0Neokun0
+
+// Changing The Property Value
+student.changeName = "Nishant";
+
+console.log(student.firstName); // Nishant
