966b1e9e01b0de9a4e53b34461cd8ff0221fe9e1
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-01 11:06:50 +0900

    Iterate Through an Object

---
+++ loops/ilerate_object_loop.js
@@ -0,0 +1,15 @@
+// OBJECTS
+const student = {
+    name: "Nishant",
+    class: "処理ネットワーク専攻",
+    porject: "Upcolor"
+}
+
+// Using for ... in
+for ( let i in student ) {
+
+    // Display The Properties
+    console.log(`${i} => ${student[i]}`);
+}
+
+// END
