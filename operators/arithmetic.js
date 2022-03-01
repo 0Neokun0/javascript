cbcfd549915eeedc299543f12b1f56f0e4d25344
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-03-01 10:07:02 +0900

    Changes In Variables

--- operators/arithmetic.js
+++ operators/arithmetic.js
@@ -4,21 +4,21 @@ let number1 = 5;
 let number2 = 3;
 
 // Addition
-console.log(`${number1} + ${number1} = ${number1 + number2}`);
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
 
 // Subtraction
-console.log(`${number1} - ${number1} = ${number1 - number2}`);
+console.log(`${number1} - ${number2} = ${number1 - number2}`);
 
 // Multiplication
-console.log(`${number1} * ${number1} = ${number1 * number2}`);
+console.log(`${number1} * ${number2} = ${number1 * number2}`);
 
 // Division
-console.log(`${number1} / ${number1} = ${number1 / number2}`);
+console.log(`${number1} / ${number2} = ${number1 / number2}`);
 
 // Remainder
-console.log(`${number1} % ${number1} = ${number1 % number2}`);
+console.log(`${number1} % ${number2} = ${number1 % number2}`);
 
 // Exponentiation
-console.log(`${number1} ** ${number1} = ${number1 ** number2}`);
+console.log(`${number1} ** ${number2} = ${number1 ** number2}`);
 
 // End
