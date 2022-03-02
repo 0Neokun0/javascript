f35a4f3b47fbbac8ad61e616a977b283470023f2
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-02 20:56:14 +0900

    String Split Javascript Program

---
+++ java-string-method/string_split.js
@@ -0,0 +1,9 @@
+// String
+const message = "JavaScript is Kinda Of Fun";
+
+// Divides The Mesage String From Space
+let result = message.split(" ");
+console.log(result);
+
+// Split Type
+console.log("ABCDEFGHI".split(""));
