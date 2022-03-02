0949a3f94edf15de1eda22c9e9a1ff2b64fd1935
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-03 08:54:14 +0900

    Compare Words JavaScript Program

---
+++ java-string-method/string_compare.js
@@ -0,0 +1,7 @@
+// Letter "h" comes after "c"
+let result = "h".localeCompare("c");
+console.log(result); // 1
+
+// Words "JavaScript" and "JavaScript" are Equivalent
+let result2 = "JavaScript".localeCompare("JavaScript");
+console.log(result2); // 0
