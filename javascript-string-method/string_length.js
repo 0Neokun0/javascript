6b220bc193c5727beb0f74e6fcb3fc1c5cfecd11
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-04 17:34:16 +0900

    String Length JavaScript Program

---
+++ javascript-string-method/string_length.js
@@ -0,0 +1,13 @@
+// JavaScript To Get Length Of Strings
+
+let sentence = "Hi";
+let length = sentence.length;
+console.log(length); // 2
+
+sentence = "That Which Is Not.";
+length = sentence.length;
+console.log(length); // 18
+
+// No Effect If Try To Assign Value To Length
+sentence.length = 5;
+console.log(sentence);
