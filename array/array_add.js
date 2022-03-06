50192448ce2a0dc76495c001dae01d2e146ee43f
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-07 08:53:52 +0900

    Add Array JavaScript Program

---
+++ array/array_add.js
@@ -0,0 +1,14 @@
+// Program To Add Element To An Array
+function addElement(arr) {
+
+    // Adding New Array Element
+    arr.unshift(4);
+
+    console.log(arr);
+}
+
+const array = [1, 2, 3];
+
+// Calling The function
+// Passing array Argument
+addElement(array);
