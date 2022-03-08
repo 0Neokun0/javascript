5a808c506a9dbc2b582c952cdc5052d8e0217c27
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-08 16:31:23 +0900

    String Concate JavaScript Program

---
+++ javascript-string-method/string_concate.js
@@ -0,0 +1,11 @@
+console.log("".concat({}));
+console.log("".concat(null));
+console.log("".concat(true));
+console.log("".concat(4, 5));
+
+let str1 = "Hello";
+let str2 = "World";
+
+// Concatenating Two Strings
+let newSrt = str1.concate(", ", str2, "!");
+console.log(newStr);
