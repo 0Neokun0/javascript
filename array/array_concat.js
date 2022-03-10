ab9681d7f3eb8fa4e940ccfe122fa7546704e823
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-11 06:16:59 +0900

    Array Joined JavaScript Program

---
+++ array/array_concat.js
@@ -0,0 +1,8 @@
+let primeNumbers = [2, 3, 5, 7, "joining"]
+let evenNumbers = [2, 4, 5, 8]
+
+// Join Two Arrays
+let joinedArrays = primeNumbers.concat(evenNumbers);
+
+// Display Joined Array
+console.log(joinedArrays);
