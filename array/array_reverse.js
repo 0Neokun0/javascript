565b4e62b75567c9b2c842b94987a7903a8cb98d
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-12 17:51:56 +0900

    Array reverse JavaScript Program

---
+++ array/array_reverse.js
@@ -0,0 +1,11 @@
+var languages = ["JavaScript", " Python", " C++", "Java", " Ruby"];
+console.log("Original Array : " + languages);
+
+// Reversing Array Order
+reversed_arr = languages.reverse();
+
+// Reverse() Modifies The Original Array
+console.log("Array After Reverse() : " + languages);
+
+// Reverse() Also Returns the Reversed Array
+console.log("Return Value of Reverse() : " + reversed_arr);
