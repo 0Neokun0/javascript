e5735cef2e751db0a3875c3485d4ddc3d41e5685
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-14 09:51:39 +0900

    Array Include JavaScript Program:

---
+++ array/array_include.js
@@ -0,0 +1,11 @@
+// Array 1
+const numbers = [1, 2, 3];
+console.log(numbers);
+console.log("Does the Element Exist : " + numbers.includes(2));
+
+// Array 2
+const pets = ['cat', 'dog', 'bat'];
+console.log(pets);
+console.log("Does the Pet Exist : " + pets.includes('cat'));
+
+
