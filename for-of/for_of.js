c9589acd07431bcccf23b572fa629b78042751af
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-19 19:06:33 +0900

    for of method

---
+++ for-of/for_of.js
@@ -0,0 +1,21 @@
+// Array
+const names = ['Vishu', 'Bhrama', 'Mahesh'];
+// Using for..of
+for ( let element of names ) {
+    console.log(element);
+}
+
+// String
+const string = 'SHIVA';
+// Using for..of loop
+for (let i of string) {
+    console.log(i);
+}
+
+// Define Set
+const set = new Set([1, 2, 3]);
+
+// Looping through Set
+for (let i of set) {
+    console.log(i);
+}
