dad61cd107d14562b0c09db5cd23f714cf19107d
0Neokun0 <nishant.meher.9595@gmail.com>
2022-03-22 18:04:57 +0900

    Number Properties

---
+++ number/number_properties.js
@@ -0,0 +1,17 @@
+// largest possible value
+const number1 = Number.MAX_VALUE;
+console.log("max possible value : " + number1); // 1.7976931348623157e+308
+
+// maximum safe integer
+const number2 = Number.MAX_SAFE_INTEGER;
+console.log("max safe interger : " + number2); // 9007199254740991
+
+const a = '23'; // string
+const b = true; // boolean
+
+//converting to number
+const result1 = Number(a);
+const result2 = Number(b);
+
+console.log(result1); // 23
+console.log(result2); // 1
