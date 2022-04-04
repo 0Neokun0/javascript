aaf78c3ce60a65afde9be3bc05b1faa371835076
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-05 07:34:46 +0900

    Iterating Over Multidimensional Array

---
+++ iterables-iterators/multidimensional_array.js
@@ -0,0 +1,8 @@
+let studentsData = [['Neokun', 28], ['Unknown', 20],];
+
+// Iterating Over The Students Data
+studentsData.forEach((student) => {
+    student.forEach((data) => {
+        console.log(data);
+    });
+});
