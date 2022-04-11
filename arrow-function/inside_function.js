a57035974f79f6723d9a4e8304eacafbad7b201e
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-12 07:29:00 +0900

    Inside Arrow Function

---
+++ arrow-function/inside_function.js
@@ -0,0 +1,16 @@
+function Person() {
+    this.name = 'Nishant',
+    this.age = 27,
+    this.sayName = function () {
+
+        console.log(this.age);
+        let innerFunc = () => {
+            console.log(this.age);
+        }
+
+        innerFunc();
+    }
+}
+
+const x = new Person();
+x.sayName();
