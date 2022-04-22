465dd018c1f6682d23f3a4eef664a3e1fad641c5
0Neokun0 <nishant.meher.9595@gmail.com>
2022-04-23 06:09:37 +0900

    Function

---
+++ constructor/function.js
@@ -0,0 +1,12 @@
+function Person(firstName, lastName) {
+    if (!new.target) {
+        return new Person(firstName, lastName);
+    }
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+let person = Person("Nishant", "0Neokun0");
+
+console.log(person.firstName);
