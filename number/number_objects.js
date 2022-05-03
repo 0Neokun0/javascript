6ace0fc7c9645a5dd0f43b13f68aeeedda165b43
0Neokun0 <nishant.meher.9595@gmail.com>
2022-05-04 07:57:14 +0900

    Number Objects

---
+++ number/number_objects.js
@@ -0,0 +1,10 @@
+const a = 45;
+
+// creating a number object
+const b = new Number(45);
+
+console.log(a); // 45
+console.log(b); // 45
+
+console.log(typeof a); // "number"
+console.log(typeof b); // "object"
