83925f0432eca353dffc31c0fe44f6e6a54e3c4e
0Neokun0 <nishant.meher.9595@gmail.com>
2022-05-07 15:20:20 +0900

    Hoisting in Javascript

---
+++ hoisting/hoisting.js
@@ -0,0 +1,12 @@
+// program to display value
+var a = 4;
+
+function greet() {
+    b = 'hello';
+    console.log(b); // hello
+    var b;
+}
+
+greet(); // hello
+console.log(b);
+console.log(a); // 5
